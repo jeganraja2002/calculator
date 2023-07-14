67a081699d3d4b2d260cfd4d010330cadb953408
jeganraja2002 <jeganraja2002@gmail.com>
2023-07-14 18:38:16 +0530

    update

---
+++ js/js.js
@@ -0,0 +1,17 @@
+
+			var  input=document.getElementById("input")
+
+
+			function num(f) {
+				input.value+=f
+			}
+
+			function clean() {
+				input.value=innerHTML=""
+			}
+		
+			function res(){
+				var math=eval(input.value)
+				input.value=math
+
+			}
